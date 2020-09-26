b73205449e687a42ac324069ba3b35e9fa86c13d
Eleatzar Colomer <3141472+ecolomer@users.noreply.github.com>
2020-09-26 23:04:10 +0200

    Remove netcat from service container entrypoint

--- docker/service/entrypoint.sh
+++ docker/service/entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# WordPress image must have "nc" available for this to work
-retries=10
-while [ "$retries" -gt 0 ] && ! nc -z $WORDPRESS_DB_HOST 3306
-do
-  sleep 2
-  ((retries--))
-done
-echo "Database ready!"
-
 echo "Adding permissions"
 chown -R www-data:www-data /var/www/html/wp-content &
 find /var/www/html/wp-content -type d -exec chmod 0755 {} \; &
