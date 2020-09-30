37be8f1dc974293a8f4153a4b3c0355379195134
Eleatzar Colomer <3141472+ecolomer@users.noreply.github.com>
2020-09-30 12:04:01 +0200

    Enable read-only root filesystem on ECS

--- docker/service/entrypoint.sh
+++ docker/service/entrypoint.sh
@@ -5,5 +5,10 @@ echo "Ensuring correct permissions ..."
 chown -R www-data:www-data /var/www/html/wp-content &
 find /var/www/html/wp-content -type d -exec chmod 0755 {} \; &
 find /var/www/html/wp-content -type f -exec chmod 0644 {} \; &
+# If we want to run a read-only root filesystem, we need to mount a
+# writable /tmp but ECS/Fargate 1.4.0 does not preserve underlying
+# permissions when mounting ephemeral task storage. This is why
+# we need to manually fixe the /tmp permissions
+chmod 1777 /tmp
 
 exec docker-entrypoint.sh "$@"
