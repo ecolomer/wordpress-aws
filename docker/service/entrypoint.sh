77699de8ebd88f2cba4d3489789f4ff939bf6045
Eleatzar Colomer <3141472+ecolomer@users.noreply.github.com>
2020-10-09 17:44:09 +0200

    Add Apache PageSpeed module

--- docker/service/entrypoint.sh
+++ docker/service/entrypoint.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo "Ensuring correct permissions ..."
-chown -R www-data:www-data /var/www/html/wp-content &
+chown -R www-data:www-data /var/www/html/wp-content /var/log/pagespeed /var/cache/mod_pagespeed &
 find /var/www/html/wp-content -type d -exec chmod 0755 {} \; &
 find /var/www/html/wp-content -type f -exec chmod 0644 {} \; &
 
